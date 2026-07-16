c86f2dc23be6663680f514e3e977f6cbdcebbcf6
TarekAhmed-MG <173894783+TarekAhmed-MG@users.noreply.github.com>
2026-07-16 15:00:52 +0100

    DC-9182: update bootstrap version and add mongo test play 30 at 2.12.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,12 +7,13 @@ import sbt.*
 
 object Dependencies {
 
-  private val bootstrapVersion = "10.7.0"
+  private val bootstrapVersion = "10.7.1"
 
   lazy val appDependencies: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion,
     "uk.gov.hmrc" %% "dc-message-library"        % "1.28.0",
     "uk.gov.hmrc" %% "bootstrap-test-play-30"    % bootstrapVersion % "test",
-    "uk.gov.hmrc" %% "domain-test-play-30"       % "13.0.0"         % "test"
+    "uk.gov.hmrc" %% "domain-test-play-30"       % "13.0.0"         % "test",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % "2.12.0"     % "test",
   )
 }
